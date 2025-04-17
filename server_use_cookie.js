455edbd92e64e7699fb13a5a5ff1203495ecd30d
caokyngo <ngocaoky96@gmail.com>
2025-04-18 00:35:39 +0700

    Add server file

--- server_use_cookie.js
+++ server_use_cookie.js
@@ -5,13 +5,14 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-app.use(cors({ origin: '*' }));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // hoặc thay * bằng domain cụ thể
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/get-video', async (req, res) => {
